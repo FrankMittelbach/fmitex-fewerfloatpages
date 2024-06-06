22d52b0b3adf7115b5819aedbb113d37c4180339
Frank Mittelbach <frank.mittelbach@latex-project.org>
2024-06-06 18:11:05 +0200

    test updates in 2024

---
+++ update-all.sh
@@ -0,0 +1,19 @@
+l3build save -epdftex \
+	fewerfloatpages-000 \
+	fewerfloatpages-001 \
+	fewerfloatpages-002 \
+	fewerfloatpages-003 \
+	fewerfloatpages-006 \
+	fewerfloatpages-007 \
+	fewerfloatpages-008 \
+	fewerfloatpages-009
+
+
+
+l3build save -epdftex,xetex,luatex \
+	fewerfloatpages-004 \
+	fewerfloatpages-005
+
+
+exit
+
